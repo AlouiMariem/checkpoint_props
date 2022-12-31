92c90ae4c6ece93a153c61533916302a73a4a8e9
AlouiMariem <[200~ --global user.name Your>
2022-12-31 02:41:12 +0100

    props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Profile from './Component/profil/Profile';
+
+import Footer from './Component/profil/Footer';
+import NavBar from './Component/profil/NavBar';
+import FullName from './Component/profil/FullName';
 
 function App() {
+ const handleName=(name)=>{alert(name)}
+  var prof = {
+     fullName :"Aloui Mariem",
+     bio : "I am a software engineer with robust problem-solving skills and developing web applications",
+     profession: "SOFTWARE ENGINEER"
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className='App'>
+    <NavBar/>
+    <FullName />
+     <Profile fullName={prof.fullName} bio={prof.bio} profession={prof.profession} handleName={handleName} >
+      <div>
+        
+        
+      
+         </div>
+     </Profile>
+     <Footer/>
+   </div>
   );
-}
+} 
+
 
 export default App;
